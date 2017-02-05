9de5c11807b7d83559dcc8fe80e68808e03df5ca
Benoît <benoit.catillon@gmail.com>
2017-02-05 21:24:09 +0100

    Modif skills & indentation

--- contact.php
+++ contact.php
@@ -1,62 +1,62 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-    <head>
+<head>
 
-        <!-- Don't forget / N'oubliez pas : stage@benoitcatillon.xyz -->
-        <meta charset="iso-8859-1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="CV de Benoît Catillon">
-        <meta name="author" content="Benoît Catillon">
-        <!-- http://benoitcatillon.xyz/wink.html -->
+    <!-- Don't forget / N'oubliez pas : stage@benoitcatillon.xyz -->
+    <meta charset="iso-8859-1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="CV de Benoît Catillon">
+    <meta name="author" content="Benoît Catillon">
+    <!-- http://benoitcatillon.xyz/wink.html -->
 
-        <link rel="shortcut icon" href="img/ico/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="img/ico/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/ico/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/ico/favicon.ico" type="image/x-icon">
 
-        <title>Benoît Catillon - Bienvenue sur mon CV</title>
+    <title>Benoît Catillon - Bienvenue sur mon CV</title>
 
-        <!-- Bootstrap Core CSS -->
-        <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
 
-        <!-- CSS Desktop & W3 -->
-        <link href="css/main.css" rel="stylesheet">
-        <!-- CSS Mobile & W3 -->
-        <link href="css/mainmob.css" rel="stylesheet" media="(max-width: 480px)" type="text/CSS">
+    <!-- CSS Desktop & W3 -->
+    <link href="css/main.css" rel="stylesheet">
+    <!-- CSS Mobile & W3 -->
+    <link href="css/mainmob.css" rel="stylesheet" media="(max-width: 480px)" type="text/CSS">
 
-        <!-- JavaScript / jQuery -->
-        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-        <script src="js/hover.zoom.js"></script>
-        <script src="js/hover.zoom.conf.js"></script>
+    <!-- JavaScript / jQuery -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/hover.zoom.js"></script>
+    <script src="js/hover.zoom.conf.js"></script>
 
-    </head>
+</head>
 
-    <body>
+<body>
 
-        <!-- Inclusion du Header -->
-        <?php include('php/header.inc.php'); ?>
+<!-- Inclusion du Header -->
+<?php include('php/header.inc.php'); ?>
 
-      	<!-- Index : WW Container Include -->
-      	<div id="ww">
-      	    <div class="container">
+<!-- Index : WW Container Include -->
+<div id="ww">
+    <div class="container">
 
-                <!-- Inclusion du Formulaire -->
-                <?php include('php/form.inc.php'); ?>
+        <!-- Inclusion du Formulaire -->
+        <?php include('php/form.inc.php'); ?>
 
-            </div> <!-- FIN container -->
-        </div><!-- FIN WW  -->
+    </div> <!-- FIN container -->
+</div><!-- FIN WW  -->
 
-        <!-- Inclusion du Footer -->
-  	    <?php include('php/footer.inc.php'); ?>
+<!-- Inclusion du Footer -->
+<?php include('php/footer.inc.php'); ?>
 
-        <!-- Bootstrap Core JavaScript -->
-        <script src="js/bootstrap.min.js"></script>
-        <!-- GitHub Button -->
-        <script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<!-- GitHub Button -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
 
-        <!-- Return-on-Top Button -->
-        <script type="text/javascript" src="js/returnontop.js"></script>
+<!-- Return-on-Top Button -->
+<script type="text/javascript" src="js/returnontop.js"></script>
 
-    </body>
+</body>
 
 </html>
--- php/skills.inc.php
+++ php/skills.inc.php
@@ -1,105 +1,95 @@
 <!-- Compétences en Programmation -->
-
 <div id="competences" class="row">
     <div id="white" class="blockprogressbar col-lg-offset-2 centered center-div">
         <h1>COMPÉTENCES</h1>
-            <table class="center-div" style="width: 65%;">
-              <tr>
+        <table class="center-div" style="width: 65%;">
+            <tr>
                 <td>
-                  <div>
-                    <p><strong>HTML/CSS</strong></p>
-                    <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
-                      <div class="w3-progressbar w3-round-large w3-green" style="width:90%">
-                        <div class="w3-center w3-text-white">90%</div>
-                      </div>
-                    </div>
-                  </div><!-- FIN progressbar HTML/CSS -->
+                    <div>
+                        <p><strong>HTML/CSS</strong></p>
+                        <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
+                            <div class="w3-progressbar w3-round-large w3-green" style="width:90%">
+                                <div class="w3-center w3-text-white">90%</div>
+                            </div>
+                        </div>
+                    </div><!-- FIN progressbar HTML/CSS -->
 
-                    <br />
+                    <br/>
 
-                  <div>
-                    <p><strong>Bootstrap / W3 CSS</strong></p>
-                    <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
-                      <div class="w3-progressbar w3-round-large w3-green" style="width:50%">
-                        <div class="w3-center w3-text-white">50%</div>
-                      </div>
-                    </div>
-                  </div><!-- FIN progressbar Bootstrap/W3 -->
+                    <div>
+                        <p><strong>Bootstrap / W3 CSS</strong></p>
+                        <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
+                            <div class="w3-progressbar w3-round-large w3-green" style="width:50%">
+                                <div class="w3-center w3-text-white">50%</div>
+                            </div>
+                        </div>
+                    </div><!-- FIN progressbar Bootstrap/W3 -->
 
-                    <br />
+                    <br/>
 
-                  <div>
-                    <p><strong>JavaScript</strong></p>
-                    <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
-                      <div class="w3-progressbar w3-round-large w3-green" style="width:50%">
-                        <div class="w3-center w3-text-white">50%</div>
-                      </div>
-                    </div>
-                  </div><!-- FIN progressbar JavaScript -->
+                    <div>
+                        <p><strong>JavaScript</strong></p>
+                        <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
+                            <div class="w3-progressbar w3-round-large w3-green" style="width:50%">
+                                <div class="w3-center w3-text-white">50%</div>
+                            </div>
+                        </div>
+                    </div><!-- FIN progressbar JavaScript -->
 
-                    <br />
+                    <br/>
 
-                  <div>
-                    <p><strong>PHP / POO</strong></p>
-                    <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
-                      <div class="w3-progressbar w3-round-large w3-green" style="width:60%">
-                        <div class="w3-center w3-text-white">60%</div>
-                      </div>
-                    </div>
-                  </div><!-- FIN progressbar PHP/POO -->
+                    <div>
+                        <p><strong>PHP / POO</strong></p>
+                        <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
+                            <div class="w3-progressbar w3-round-large w3-green" style="width:60%">
+                                <div class="w3-center w3-text-white">60%</div>
+                            </div>
+                        </div>
+                    </div><!-- FIN progressbar PHP/POO -->
 
-                    <br />
+                    <br/>
 
-                  <div>
-                    <p><strong>Symfony 2.8</strong></p>
-                    <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
-                      <div class="w3-progressbar w3-round-large w3-green" style="width:30%">
-                        <div class="w3-center w3-text-white">30%</div>
-                      </div>
-                    </div>
-                  </div><!-- FIN progressbar Symfony -->
+                    <div>
+                        <p><strong>Symfony 2.8</strong></p>
+                        <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
+                            <div class="w3-progressbar w3-round-large w3-green" style="width:30%">
+                                <div class="w3-center w3-text-white">30%</div>
+                            </div>
+                        </div>
+                    </div><!-- FIN progressbar Symfony -->
 
-                    <br />
+                    <br/>
 
-                  <div>
-                    <p><strong>MySQL</strong></p>
-                    <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
-                      <div class="w3-progressbar w3-round-large w3-green" style="width:60%">
-                        <div class="w3-center w3-text-white">60%</div>
-                      </div>
-                    </div>
-                  </div><!-- FIN progressbar MySQL -->
+                    <div>
+                        <p><strong>MySQL</strong></p>
+                        <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
+                            <div class="w3-progressbar w3-round-large w3-green" style="width:60%">
+                                <div class="w3-center w3-text-white">60%</div>
+                            </div>
+                        </div>
+                    </div><!-- FIN progressbar MySQL -->
 
-                    <br />
+                    <br/>
 
-                  <div>
-                    <p><strong>Java SE</strong></p>
-                    <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
-                        <div class="w3-center w3-text-black">En cours...</div>
-                    </div>
-                  </div><!-- FIN progressbar Java SE -->
+                    <div>
+                        <p><strong>Java SE</strong></p>
+                        <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
+                            <div class="w3-center w3-text-black">En cours...</div>
+                        </div>
+                    </div><!-- FIN progressbar Java SE -->
 
-                    <br />
+                    <br/>
 
-                  <div>
-                    <p><strong>Java EE</strong></p>
-                    <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
-                        <div class="w3-center w3-text-black">En cours...</div>
-                    </div>
-                  </div><!-- FIN progressbar Java EE -->
-
-                    <br />
-
-                  <div>
-                    <p><strong>Android Natif</strong></p>
-                    <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
-                        <div class="w3-center w3-text-black">Prochainement...</div>
-                    </div>
-                </div><!-- FIN progressbar Android -->
-              </td>
+                    <div>
+                        <p><strong>Android Natif</strong></p>
+                        <div class="w3-progress-container w3-round-large w3-small" style="width:100%">
+                            <div class="w3-center w3-text-black">Prochainement...</div>
+                        </div>
+                    </div><!-- FIN progressbar Android -->
+                </td>
             </tr>
-          </table>
+        </table>
 
-        <br />
+        <br/>
     </div><!-- FIN white blockprogressbar -->
 </div><!-- FIN competences row -->
